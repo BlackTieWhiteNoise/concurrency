834a8d7cd875ccfefb3fec93ec10a92f2188e3ef
btwn <prot0prot@proton.me>
2023-07-07 17:32:43 -0700

    Run methods in diff threads

--- src/main/java/course/concurrency/m2_async/executors/spring/AsyncClassTest.java
+++ src/main/java/course/concurrency/m2_async/executors/spring/AsyncClassTest.java
@@ -3,13 +3,9 @@ package course.concurrency.m2_async.executors.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 @Component
 public class AsyncClassTest {
@@ -17,19 +13,14 @@ public class AsyncClassTest {
     @Autowired
     public ApplicationContext context;
 
-
     @Autowired
-    ApplicationConfiguration configuration;
+    @Qualifier("applicationTaskExecutor")
+    private ThreadPoolTaskExecutor executor;
 
-    SimpleAsyncTaskExecutor simpleAsyncTaskExecutor = new SimpleAsyncTaskExecutor();
-
-    public void runTasksWithExecutor() {
-        simpleAsyncTaskExecutor.execute(() -> runAsyncTask());
-    }
-
+    @Async
     public void runAsyncTask() {
         System.out.println("runAsyncTask: " + Thread.currentThread().getName());
-        simpleAsyncTaskExecutor.execute(() -> internalTask());
+        executor.execute(() -> internalTask());
     }
 
     public void internalTask() {
